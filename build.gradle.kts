81e8bcfc3c18b0d8ced637e285b3f321eb325005
Christophe Beyls <software@digitalia.be>
2023-12-21 11:59:01 +0100

    prepare release 0.8.1 and upgrade maven publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     kotlin("multiplatform") version "1.9.10" apply false
     id("org.jetbrains.compose") version "1.5.11" apply false
-    id("com.vanniktech.maven.publish") version "0.25.3" apply false
+    id("com.vanniktech.maven.publish") version "0.26.0" apply false
     id("com.android.application") version "8.2.0" apply false
 }
