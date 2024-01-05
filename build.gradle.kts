11b573fba27ab3ca99eae83673fd292a40983a6d
Christophe Beyls <software@digitalia.be>
2024-01-05 14:40:05 +0100

    chore: upgrade Kotlin to 1.9.21 and Android Gradle plugin to 8.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("multiplatform") version "1.9.10" apply false
+    kotlin("multiplatform") version "1.9.21" apply false
     id("org.jetbrains.compose") version "1.5.11" apply false
     id("com.vanniktech.maven.publish") version "0.26.0" apply false
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.1" apply false
 }
