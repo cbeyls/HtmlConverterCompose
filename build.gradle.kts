19740489235434c4bf15d18b84267d55f0161412
Christophe Beyls <software@digitalia.be>
2024-01-22 16:00:57 +0100

    chore: update maven publish plugin to 0.27.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     kotlin("multiplatform") version "1.9.21" apply false
     id("org.jetbrains.compose") version "1.5.11" apply false
-    id("com.vanniktech.maven.publish") version "0.26.0" apply false
+    id("com.vanniktech.maven.publish") version "0.27.0" apply false
     id("com.android.application") version "8.2.1" apply false
 }
