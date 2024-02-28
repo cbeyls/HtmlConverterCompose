5638078b30a60d94206d7e19af4fb721a1997860
Christophe Beyls <software@digitalia.be>
2024-02-28 12:35:40 +0100

    chore: upgrade to Compose Multiplatform 1.6.0 and Kotlin 1.9.22

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("multiplatform") version "1.9.21" apply false
-    id("org.jetbrains.compose") version "1.5.11" apply false
+    kotlin("multiplatform") version "1.9.22" apply false
+    id("org.jetbrains.compose") version "1.6.0" apply false
     id("com.vanniktech.maven.publish") version "0.27.0" apply false
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "8.2.2" apply false
 }
