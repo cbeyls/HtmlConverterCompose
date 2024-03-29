6cbb3660c43acac196979e13a1790a06e53de067
Christophe Beyls <software@digitalia.be>
2024-03-29 15:46:34 +0100

    chore: update Kotlin to 1.9.23, Compose to 1.6.1, Gradle to 8.4, AGP to 8.3.1, Maven publish plugin to 0.28.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("multiplatform") version "1.9.22" apply false
-    id("org.jetbrains.compose") version "1.6.0" apply false
-    id("com.vanniktech.maven.publish") version "0.27.0" apply false
-    id("com.android.application") version "8.2.2" apply false
+    kotlin("multiplatform") version "1.9.23" apply false
+    id("org.jetbrains.compose") version "1.6.1" apply false
+    id("com.vanniktech.maven.publish") version "0.28.0" apply false
+    id("com.android.application") version "8.3.1" apply false
 }
