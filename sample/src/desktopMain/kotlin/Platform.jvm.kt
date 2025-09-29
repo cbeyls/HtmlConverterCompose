416046b6e80b5755322c03e93b6fc2bc41ed8a2c
Goooler <wangzongler@gmail.com>
2025-09-29 15:34:36 +0800

    Private actual Platform classes

--- sample/src/desktopMain/kotlin/Platform.jvm.kt
+++ sample/src/desktopMain/kotlin/Platform.jvm.kt
@@ -1,4 +1,4 @@
-class JVMPlatform: Platform {
+private class JVMPlatform: Platform {
     override val name: String = "Java ${System.getProperty("java.version")}"
 }
 
--- sample/src/iosMain/kotlin/Platform.ios.kt
+++ sample/src/iosMain/kotlin/Platform.ios.kt
@@ -1,6 +1,6 @@
 import platform.UIKit.UIDevice
 
-class IOSPlatform: Platform {
+private class IOSPlatform: Platform {
     override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
